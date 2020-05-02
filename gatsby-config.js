36e56c24409f4b90fb9b4a5a1f0c5395b00213d9
Gunnar Grosch <gunnar@grosch.se>
2020-05-02 09:47:50 +0200

    Removed gatsby-plugin-offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,7 +44,6 @@ module.exports = {
         icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
       },
     },
-    'gatsby-plugin-sass',
-    'gatsby-plugin-offline'
+    'gatsby-plugin-sass'
   ],
 }
