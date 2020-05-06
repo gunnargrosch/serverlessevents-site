dd463e6794d93188095594011c316fe72ad47d6a
Gunnar Grosch <gunnar@grosch.se>
2020-05-06 17:50:25 +0200

    Updated manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,8 +35,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Serverless Events',
+        short_name: 'serverlessevents.io',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
