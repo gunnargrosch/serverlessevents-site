cf2279e0510b5af1f28378fd688f6539a81a6d9f
Gunnar Grosch <gunnar@grosch.se>
2020-05-08 19:05:17 +0200

    Hide epoch fields

--- src/components/AddEventForm.js
+++ src/components/AddEventForm.js
@@ -91,7 +91,7 @@ class AddEventForm extends Component {
                       timeCaption="time"
                       dateFormat="MMMM d, yyyy h:mm aa"
                     />
-                    <input type="text" pattern="[0-9]*" name="starttime" id="starttime" required onChange={this.handleFields} value={this.state.starttime} />
+                    <input type="hidden" pattern="[0-9]*" name="starttime" id="starttime" required onChange={this.handleFields} value={this.state.starttime} />
                 </div>
                 <div className="field half">
                     <label htmlFor="endtimeLong">End time ({moment.tz(moment.tz.guess()).zoneAbbr()})</label>
@@ -111,7 +111,7 @@ class AddEventForm extends Component {
                       timeCaption="time"
                       dateFormat="MMMM d, yyyy h:mm aa"
                     />
-                    <input type="text" pattern="[0-9]*" name="endtime" id="endtime" required onChange={this.handleFields} value={this.state.endtime} />
+                    <input type="hidden" pattern="[0-9]*" name="endtime" id="endtime" required onChange={this.handleFields} value={this.state.endtime} />
                 </div>
                 <div className="field half first">
                     <label htmlFor="eventlocation">Location (Online or city and country)</label>
